b95008642a89299f507d78693e549f4f3539979d
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-10-26 10:21:42 +0530

    using useContext()

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useContext} from "react";
+import AuthContext from "../store/auth-context";
+import Button from "../UI/Button/Button";
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
 
+// In Button we could use context instead of props, it works. But props is ok here
+// becuase we are directly using the prev props in this component rather than passing
+// it to whole new component
 const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Log Out</Button>     
     </Card>
   );
 };
